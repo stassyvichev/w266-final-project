793c027b07e36419ad230db09d6ff85d5ba93d1d
Stanimir Vichev <stassyvichev@gmail.com>
2018-07-17 21:21:39 +0000

    new branch for my work

--- graph.py
+++ graph.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 def affineLayer(hidden_dims, x):
     W = tf.get_variable("W", shape=[x.get_shape().as_list()[1], hidden_dim],initializer=tf.contrib.layers.xavier_initializer())
     b = tf.get_variable(name="b", shape=(hidden_dim), initializer=tf.zeros_initializer())
-    return tf.add(tf.matmul(x,W),b) 
+    return tf.add(tf.matmul(x,W),b)
+
+def newFunc():
+    pass 
