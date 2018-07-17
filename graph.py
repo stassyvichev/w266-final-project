2fc6061ccabebe905c519bf175984ca25d657a27
Stanimir Vichev <stassyvichev@gmail.com>
2018-07-17 21:17:18 +0000

    added start of feed forward nn code

---
+++ graph.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+def affineLayer(hidden_dims, x):
+    W = tf.get_variable("W", shape=[x.get_shape().as_list()[1], hidden_dim],initializer=tf.contrib.layers.xavier_initializer())
+    b = tf.get_variable(name="b", shape=(hidden_dim), initializer=tf.zeros_initializer())
+    return tf.add(tf.matmul(x,W),b) 
